8ca648e25be88cbe01498b36105b010b40c2a0f2
jai-tanwar2005 <jaitanwar327@gmail.com>
2026-07-15 17:48:53 +0530

    Time: 30 ms (82%), Space: 9.8 MB (78.29%) - LeetHub

---
+++ 2947-count-beautiful-substrings-i/2947-count-beautiful-substrings-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' ||
+               ch == 'o' || ch == 'u';
+    }
+
+    int beautifulSubstrings(string s, int k) {
+        int n = s.size();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int vowel = 0;
+            int consonant = 0;
+
+            for (int j = i; j < n; j++) {
+
+                if (isVowel(s[j]))
+                    vowel++;
+                else
+                    consonant++;
+
+                if (vowel == consonant &&
+                    (vowel * consonant) % k == 0) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
